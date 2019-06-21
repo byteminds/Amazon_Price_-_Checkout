ee1983c6aca21c535bba6cd71cb0fea845d656c3
Keenan Thomson <keenan@Keenans-MacBook-Air.local>
2019-06-21 08:24:41 -0400

    Add server & database files

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3001;
+
+let app = express();
+
+app.use(express.static('../public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => `Server listening on port: ${port}`);
