7962ce096f89bf772b634c1d8fb6a3fa5fca3f06
Keenan Thomson <keenan@Keenans-MacBook-Air.local>
2019-06-22 14:40:34 -0400

    Make progress with seed script and database setup

--- database/index.js
+++ database/index.js
@@ -1,4 +1,30 @@
-const db = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb:localhost/fetcher');
+
+let repoSchema = {
+  id: Number,
+  Price: Number,
+  isPrime: String,
+  stockQty: Number,
+  shipCost: Number,
+  soldBy: String,
+  subscriptionProtectionPlanCost: Number,
+  TwoYrProtectionPlanCost: Number
+}
+
+let db = {};
+
+let Repo = mongoose.model('Repo', repoSchema);
+
+db.save = (array, err, callback) => {
+  if (err) {
+    console.log(`db.save error!`);
+  }
+  Repo.
+};
+
+db.getDocument = (id, err, callback) => {
+  console.log(`This function is not defined yet!`)
+};
 
 module.exports.db = db;
--- database/seed.js
+++ database/seed.js
@@ -2,9 +2,46 @@ const {db} = require('database/index.js');
 
 console.log(`RETURN DB --> `, db);
 
-let entries = 100;
+// let repoSchema = {
+//   id: Number,
+//   Price: Number,
+//   isPrime: String,
+//   stockQty: Number,
+//   shipCost: Number,
+//   soldBy: String,
+//   subscriptionProtectionPlanCost: Number,
+//   TwoYrProtectionPlanCost: Number
+// }
 
-for (i = 1; i <= entries; i++) {
-  let id = i;
-  let price = (100 * Math.random());
-}
+let fakeEntryCount = 100;
+let entriesArray = [];
+
+for (i = 1; i <= fakeEntryCount; i++) {
+  let obj = {
+    id: i,
+    Price: (100 * Math.random()),
+    isPrime: () => {
+
+    },
+    stockQty: () => {
+
+    },
+    shipCost: () => {
+
+    },
+    soldBy: () => {
+
+    },
+    subscriptionProtectionPlanCost: () => {
+
+    },
+    TwoYrProtectionPlanCost: () => {
+
+    }
+  }
+  entriesArray.push(obj);
+};
+
+db.save(entriesArray, () => {
+  console.log(`Fake entries saved!`);
+});
