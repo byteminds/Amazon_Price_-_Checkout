93ee58e9948bfa7982666a9caf149c8d7313ff7a
Keenan Thomson <keenan@Keenans-MacBook-Air.local>
2019-06-27 22:48:31 -0400

    Work on getting 2nd DB test to work

--- test/db.test.js
+++ test/db.test.js
@@ -1,27 +1,38 @@
 const mongoose = require('mongoose');
 
-describe('insert', () => {
+describe('database', () => {
   let db;
+  let repos;
+  let connection;
 
   beforeAll(async () => {
     connection = await mongoose.connect('mongodb://127.0.0.1/pricingComponentDB', {useNewUrlParser: true});
     db = await mongoose.connection;
+    repos = await db.collection('pricingComponentDB');
   });
 
   afterAll(async (done) => {
-
+    // db.dropDatabase();
     db.close();
     done();
   });
 
   it('should insert a doc into collection', async () => {
 
-    const repos = db.collection('pricingComponentDB');
-    const mockUser = {_id: 'some-user-id', name: 'Keenan'};
-    await repos.deleteOne(mockUser);
-    await repos.insertOne(mockUser);
+    // let repos = db.collection('pricingComponentDB');
+    const mockData = {_id: 'some-user-id', name: 'Keenan'};
+    await repos.deleteOne(mockData);
+    await repos.insertOne(mockData);
 
-    const insertedUser = await repos.findOne({id: 'some-user-id'});
-    expect(insertedUser).toEqual(mockUser);
+    const insertedUser = await repos.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockData);
+  });
+
+  it('should contain at least 100 records', async () => {
+    // let repos = await db.collection('pricingComponentDB');
+    let records = await repos.find({}).lean().exec((err, docs) => {
+      docs = docs.map(o => o.toObject())
+    });
+    expect(records.length).toBeGreaterThanOrEqual(100);
   });
 });
