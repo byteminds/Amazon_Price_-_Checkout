3334989bb978a0ee3225667e35e11f6cb1b50016
Keenan Thomson <keenanthomson@gmail.com>
2019-07-30 23:26:33 -0400

    Add express compression

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {db} = require('../database/index')
+const compression = require('compression');
+const {db} = require('../database/index');
 const path = require('path');
 const cors = require('cors');
 const port = 80;
@@ -8,6 +9,7 @@ const port = 80;
 let app = express();
 
 app.use(cors());
+app.use(compression());
 app.use('/:id/', express.static(path.join(__dirname, `../client/dist`)));
 app.use(bodyParser.urlencoded({ extended: true }));
 
